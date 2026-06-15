3e8410907225867b1c98de03d6d86d07974ecd0c
stitchandsnare-web <stitchandsnare@gmail.com>
2026-06-15 15:14:26 -0400

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
+
 const supabaseUrl = 'https://ysdxycbqkzfnxazjqpit.supabase.co'
 const supabaseKey = 'sb_publishable_H2FnQ3ahvEE826X945wVEw_YQk-hx-P'
-export const supabase = createClient(supabaseUrl, supabaseKey)
+
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    flowType: 'implicit',
+  }
+})
